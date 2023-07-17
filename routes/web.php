773a3b596678aeba51cc3b97c289bdf4d5c8ff47
Ilham Gumilang <gumilang.dev@gmail.com>
2023-07-18 03:07:05 +0700

    testing job

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Jobs\DummyJob;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/send-email', function () {
+    DummyJob::dispatch();
+    return 'Email sent';
 });
